ead5719f0028c4d23379d45cb31b01226448f4c2
orion <orion@192.168.1.34>
2016-07-17 11:58:25 +0200

    Basic Hello World web app using Spring, Java EE and Thymeleaf

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author orion
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    /*The @RequestMapping annotation ensures that HTTP requests to /greeting are mapped to the greeting() method.*/
+    /* @RequestMapping maps all HTTP operations by default. Use @RequestMapping(method=GET) to narrow this mapping.*/
+  
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
